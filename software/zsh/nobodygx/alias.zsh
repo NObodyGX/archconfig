392f147d3383d0b9d5dfdde73779126e8a24deda
NObodyGX <nobodygx@163.com>
2024-08-14 16:01:19 +0800

    add jpegtim

--- software/zsh/nobodygx/alias.zsh
+++ software/zsh/nobodygx/alias.zsh
@@ -12,6 +12,7 @@ alias files="nautilus "
 alias kk="du -sh *"
 alias gitea_run="gitea web -c /code/code_repos/gitea_env/gitea_home/app.ini"
 alias hexo_run="hexo cl && hexo s"
+alias hexo_gen="cd /data/shome && hexo gen && cd -"
 alias G="git clone --depth=1 "
 
 alias cp='cp -iv'
@@ -34,4 +35,22 @@ function mvimg() {
     local src="$1"
     local dst="/data/zres/images"
     mv "$src" "$dst"
+}
+
+function jpegtim() {
+    local src="$1"
+    local ori="$1/old"
+    if [ ! -d "$ori" ];then
+        mkdir -p "$ori"
+    fi
+    for tf in $(ls $src);do
+        if [[ $tf == *.jpg ]];then
+            mv -f "${src}/${tf}" "${ori}/${tf}"
+        fi
+    done
+    for tf in $(ls $ori);do
+        if [[ $tf == *.jpg ]];then
+            jpegoptim  -m75  "${ori}/${tf}" -d "$src"
+        fi
+    done
 }
